3b23843fa9aa3de3804b58dba753fe4f948a30b9
Kranthi <mkranthi18@gmail.com>
2018-06-06 20:25:11 -0400

    Initial commit

---
+++ gridtest/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gridtest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            billInput.TextChanged += (s, e) => CalculateTip();
+        }
+        void CalculateTip()
+        {
+            double bill;
+
+            if (Double.TryParse(billInput.Text, out bill) && bill > 0)
+            {
+                double tip = Math.Round(bill * 0.15, 2);
+                double final = bill + tip;
+
+                tipOutput.Text = tip.ToString("C");
+                totalOutput.Text = final.ToString("C");
+            }
+        }
+
+        void OnLight(object sender, EventArgs e)
+        {
+            Resources["fgColor"] = Color.FromHex("#606060");
+            Resources["bgColor"] = Color.FromHex("#c0c0c0");
+          
+        }
+
+        void OnDark(object sender, EventArgs e)
+        {
+            Resources["fgColor"] = Color.FromHex("#c0c0c0");
+            Resources["bgColor"] = Color.FromHex("#606060");
+        }
+
+        void GotoCustom(object sender, EventArgs e)
+        {
+           // Navigation.PushAsync(new CustomTipPage());
+        }
+    }
+
+}
